ca35d16e090d92e8039d28c690c9b816c21c7ed1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-08-14 19:58:12 +0700

    feat: Add lodash types dependency and update lodash version
    This commit adds the "@types/lodash" dependency to the project, which provides TypeScript type definitions for the lodash library. Additionally, it updates the lodash version to "^4.17.21". These changes enhance the development experience by enabling better type checking and utilizing the latest features and improvements in lodash.
    
    Note: Please remove the meta information (e.g., diff, index, etc.) from the commit message before using it.

--- src/features/OtherCities/components/OtherCitites-widget.tsx
+++ src/features/OtherCities/components/OtherCitites-widget.tsx
@@ -1,29 +1,30 @@
 import { Card, CardContent } from '@/components/ui/card';
 import { useGetOtherCities } from '../api/other-cities.datas';
+import _ from 'lodash';
 
 const OtherCitiesWidget = () => {
   const result = useGetOtherCities();
   return (
     <>
-      {result.map((item) => (
-        <Card className="h-full" key={item.data.name}>
+      {result?.map((item) => (
+        <Card className="h-full" key={item.data?.name}>
           <CardContent className="h-full">
             <div className="flex justify-between h-full">
               <div className="flex flex-col justify-between">
                 <div>
-                  <h1 className="text-sm">{item.data.sys.country}</h1>
-                  <p className="text-md font-bold">{item.data.name}</p>
+                  <h1 className="text-sm">{item.data?.sys?.country}</h1>
+                  <p className="text-md font-bold">{item.data?.name}</p>
                 </div>
-                <div> {item.data.weather[0].description}</div>
+                <div>{_.startCase(item.data?.weather[0].description)}</div>
               </div>
 
               <div className="flex flex-col justify-between items-center">
                 <img
-                  src={`https://openweathermap.org/img/wn/${item.data.weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${item.data?.weather[0].icon}@2x.png`}
                   alt="weather-icon"
                 />
                 <div className="text-md">
-                  {Math.round(item.data.main.temp)}&deg;
+                  {Math.round(item.data?.main.temp)}&deg;
                 </div>
               </div>
             </div>
